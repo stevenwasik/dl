5fc17c900fca6455ac2707e837a8fcc9d121d5e4
Steve <steven.l.wasik@gmail.com>
2019-01-01 13:04:44 -0800

    Large update with re-designed data structure for running nn.

---
+++ dl_nn.py
@@ -0,0 +1,47 @@
+import numpy as np
+import dl_helper_functions as hf
+
+
+def nn(target, predictors, classes, hidden_layer_nodes, hidden_layers, predictor_count, eps=0.001, epochs=100):
+    network_struc = [predictor_count] + [hidden_layer_nodes] * hidden_layers + [classes]
+    num_obs = len(target)
+    cost = 0
+
+    # Initialize
+    layer_dat = [[0, 0, 0, predictors]]
+    for i in range(1, len(network_struc)):
+        node_count_prior_layer = network_struc[i - 1]
+        node_count_current_layer = network_struc[i]
+        b = np.reshape(np.random.randn(node_count_current_layer), (1, node_count_current_layer))
+        w = np.reshape(np.random.randn(node_count_prior_layer * node_count_current_layer),
+                       (node_count_prior_layer, node_count_current_layer))
+        z = np.zeros((num_obs, node_count_current_layer))
+        a = np.zeros((num_obs, node_count_current_layer))
+        layer_dat.append([b, w, z, a])
+
+    for i in range(epochs):
+        # Forward Step
+        for k in range(1, len(layer_dat)):
+            layer_dat[k][2] = np.matmul(layer_dat[k - 1][3], layer_dat[k][1]) + layer_dat[k][0]
+            layer_dat[k][3] = hf.softmax(layer_dat[k][2])
+
+        cost = np.mean((target - layer_dat[-1][3]) ** 2)
+
+        # Back Step
+        del_l = -2 * (target - layer_dat[-1][3]) * hf.softmax_del(layer_dat[-1][2], target)
+        del_l_b = np.sum(del_l, keepdims=True, axis=0) / num_obs
+        del_l_w = np.sum(np.matmul(np.transpose(layer_dat[-2][3]), del_l), keepdims=True, axis=0) / num_obs
+        layer_dat[-1][0] = layer_dat[-1][0] - eps * del_l_b
+        layer_dat[-1][1] = layer_dat[-1][1] - eps * del_l_w
+
+        for j in range(2, len(layer_dat)):
+            del_lh = np.matmul(del_l, layer_dat[-j + 1][1]) * hf.sigmoid_del(layer_dat[-j][2])
+            del_l_b = np.sum(del_lh, keepdims=True, axis=0) / num_obs
+            del_l_w = np.sum(np.matmul(np.transpose(layer_dat[-j - 1][3]), del_lh), keepdims=True, axis=0) / num_obs
+            layer_dat[-j][0] = layer_dat[-j][0] - eps * del_l_b
+            layer_dat[-j][1] = layer_dat[-j][1] - eps * del_l_w
+
+        if i % int(epochs / 10) == 0:
+            print('iteration ', i, ' cost: ', cost)
+
+    return cost, layer_dat
--- test_1.py
+++ test_1.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 import pandas as pd
-import dl_ffnn
+import dl_nn
 
-c0_mean = [2, -2]
+c0_mean = [2, 2]
 c0_cov = [[1, 0], [0, 1]]
 c0 = np.random.multivariate_normal(c0_mean, c0_cov, 500)
 c0_df = pd.DataFrame(c0)
@@ -13,7 +13,6 @@ c0_df['group'] = 0.0
 c0_df['t0'] = 1.0
 c0_df['t1'] = 0.0
 c0_df['t2'] = 0.0
-c0_df['t3'] = 0.0
 
 c1_mean = [-2, -2]
 c1_cov = [[1, 0], [0, 1]]
@@ -24,51 +23,45 @@ c1_df['group'] = 1.0
 c1_df['t0'] = 0.0
 c1_df['t1'] = 1.0
 c1_df['t2'] = 0.0
-c1_df['t3'] = 0.0
 
-c2_mean = [2, 2]
+c2_mean = [2, -2]
 c2_cov = [[1, 0], [0, 1]]
 c2 = np.random.multivariate_normal(c2_mean, c2_cov, 500)
 c2_df = pd.DataFrame(c2)
 c2_df.columns = ['x', 'y']
-c2_df['group'] = 1.0
+c2_df['group'] = 2.0
 c2_df['t0'] = 0.0
-c2_df['t1'] = 1.0
-c2_df['t2'] = 0.0
-c2_df['t3'] = 0.0
+c2_df['t1'] = 0.0
+c2_df['t2'] = 1.0
 
 c3_mean = [-2, 2]
 c3_cov = [[1, 0], [0, 1]]
 c3 = np.random.multivariate_normal(c3_mean, c3_cov, 500)
 c3_df = pd.DataFrame(c3)
 c3_df.columns = ['x', 'y']
-c3_df['group'] = 0.0
-c3_df['t0'] = 1.0
-c3_df['t1'] = 0.0
-c3_df['t2'] = 0.0
-c3_df['t3'] = 0.0
+c3_df['group'] = 2.0
+c3_df['t0'] = 0.0
+c3_df['t1'] = 1.0
 
-dat = pd.concat([c0_df, c1_df, c2_df, c3_df], ignore_index=True)
+dat = pd.concat([c0_df, c1_df, c2_df], ignore_index=True)
 
-# dat.plot.scatter(x='x', y='y', c='group', cmap='coolwarm')
+dat.plot.scatter(x='x', y='y', c='group', cmap='coolwarm')
 
-target = np.array(dat[['t0', 't1']])
+# print(dat)
+
+target = np.array(dat[['t0', 't1', 't2']])
 predictors = np.array(dat[['x', 'y']])
 
 hidden_layers = 2
 hidden_layer_nodes = 3
-classes = 2
+classes = 3
 predictor_count = 2
 
-# a_in, a_hidden, a_out, w_in, w_hidden, w_out, b_in, b_hidden, b_out = dl_ffnn.ffnn(target, predictors, classes,
-#                                                                                   hidden_layer_nodes, hidden_layers,
-#                                                                                   predictor_count, eps=0.001,
-#                                                                                   epochs=100)
+cost, layer_dat = dl_nn.nn(target, predictors, classes, hidden_layer_nodes, hidden_layers, predictor_count, eps=0.005,
+                           epochs=10000)
 
-# dat['pred_0'] = np.round(a_out[:, 0], 0)
+dat['pred_group'] = np.argmax(layer_dat[-1][3], axis=1)
 
-# dat['pred_desc'] = dat[['t0', 'pred_0']].apply(lambda df: 1 if df['t0'] == df['pred_0'] else 0, axis=1)
+# print(dat.groupby(['t0', 'pred_0'])['pred_desc'].count())
 
-# dat.groupby(['t0', 'pred_0'])['pred_desc'].count()
-
-# dat.plot.scatter(x='x', y='y', c='pred_0', cmap='coolwarm')
+dat.plot.scatter(x='x', y='y', c='pred_group', cmap='coolwarm')
